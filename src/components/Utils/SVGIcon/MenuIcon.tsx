26ca4ab48592cf3d71e59cdac1551003a5790040
Taufik Oktama <oktamataufik@gmail.com>
2020-02-23 13:10:03 +0700

    Create SVGs component

---
+++ src/components/Utils/SVGIcon/MenuIcon.tsx
@@ -0,0 +1,14 @@
+const MenuIcon: React.FC<{}> = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="18"
+    height="18"
+    viewBox="0 0 24 24"
+    style={{ left: 10 }}
+    fill="currentColor"
+  >
+    <path d="M4 8h4V4H4v4zm6 12h4v-4h-4v4zm-6 0h4v-4H4v4zm0-6h4v-4H4v4zm6 0h4v-4h-4v4zm6-10v4h4V4h-4zm-6 4h4V4h-4v4zm6 6h4v-4h-4v4zm0 6h4v-4h-4v4z"></path>
+  </svg>
+);
+
+export default MenuIcon;
