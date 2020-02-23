254e12c250335d13334a63a268bc7e0be77b58be
Taufik Oktama <oktamataufik@gmail.com>
2020-02-23 20:51:09 +0700

    Remove spring transition

--- src/components/Utils/Header/ToggleModule/index.tsx
+++ src/components/Utils/Header/ToggleModule/index.tsx
@@ -1,62 +1,26 @@
-import { useToggleContext } from "../../../../lib/context/ToggleContext";
-import { useRef } from "react";
-import { useSpring, useTransition, useChain, animated } from "react-spring";
-import { useOnClickOutside } from "../../../../lib/hooks/useOnClickOutside";
-
 const ToggleModule: React.FC<{
   onClick: () => void;
   state: boolean;
   label: JSX.Element;
 }> = ({ onClick, state, label, children }) => {
-  const { dispatch } = useToggleContext();
-  const springRef = useRef();
-  const proping = useSpring({
-    ref: springRef,
-    config: { mass: 1, tension: 500, friction: 50 },
-    from: { width: 40, height: 40, border: "1px solid" },
-    to: {
-      width: state ? 320 : 40,
-      height: state ? 400 : 40,
-      border: "1px solid"
-    }
-  });
-  const transRef = useRef();
-  const transitions = useTransition(state, null, {
-    ref: transRef,
-    config: { mass: 1, tension: 500, friction: 50 },
-    from: { opacity: 0, borderTop: "1px solid" },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 }
-  });
-
-  useChain([springRef, transRef]);
-
-  const wrapper = useRef();
-  useOnClickOutside(wrapper, () => dispatch({ type: "ALL_FALSE" }));
-
   return (
-    <div ref={wrapper} style={{ position: "relative" }}>
-      <animated.div style={proping}>
-        <button disabled={state ? true : false} onClick={onClick}>
-          {label}
-        </button>
-        {transitions.map(
-          ({ item, key, props }) =>
-            item && (
-              <animated.div key={key} style={props}>
-                {children}
-              </animated.div>
-            )
-        )}
-      </animated.div>
+    <div
+      style={{
+        marginRight: -1,
+        height: 40,
+        width: state ? 320 : 40,
+        borderBottom: "1px solid",
+        position: "relative",
+        transition: "width 300ms cubic-bezier(1,0,0,1)"
+      }}
+    >
+      <button onClick={onClick}>{label}</button>
 
+      {state && children}
       <style jsx>{`
-        div {
-          margin-right: -1px;
-        }
         button {
-          width: 38px;
-          height: 38px;
+          width: 40px;
+          height: 40px;
           background: none;
           border: none;
           color: currentColor;
@@ -66,6 +30,7 @@ const ToggleModule: React.FC<{
           display: flex;
           align-items: center;
           justify-content: center;
+          border: 1px solid;
         }
       `}</style>
     </div>
