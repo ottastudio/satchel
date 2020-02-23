b92e4c49b994d243ffe66ea753d9f20e0f783112
Taufik Oktama <oktamataufik@gmail.com>
2020-02-23 13:10:42 +0700

    Create useOnClickOutside hook

---
+++ src/lib/hooks/useOnClickOutside.tsx
@@ -0,0 +1,20 @@
+import { RefObject, useEffect } from "react";
+
+export const useOnClickOutside = (
+  ref: RefObject<any>,
+  handler: (event: any) => void
+) => {
+  useEffect(() => {
+    const listener = (event: any) => {
+      if (!ref.current || ref.current.contains(event.target)) return;
+      handler(event);
+    };
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+};
