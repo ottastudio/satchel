5e4677275470dbf7326cfa1b737d8e67bfaed897
Taufik Oktama <oktamataufik@gmail.com>
2020-02-24 11:03:09 +0700

    Create footnote for account module

---
+++ src/components/Utils/Header/Modules/AccountModule/Note.tsx
@@ -0,0 +1,12 @@
+const Note: React.FC<{}> = () => (
+  <p className="account-notes">
+    If you already have an account, you can login by fill in first and second
+    forms above.
+    <br />
+    <br />
+    Or creating new account is easy. Just fill in those third, fourth, and fifth
+    forms above.
+  </p>
+);
+
+export default Note;
