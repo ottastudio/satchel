379ca3ca226dae284f55fd76d2e370326c8204b1
Taufik Oktama <oktamataufik@gmail.com>
2020-02-25 01:09:12 +0700

    Test fetching data from api routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,46 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import useRequest from "../lib/hooks/useRequest";
 import Main from "../components/Utils/Main";
 
 const Index: NextPage<{}> = () => {
+  const { data: users } = useRequest<
+    [
+      {
+        _id: string;
+        name: { first_name: string; last_name: string };
+        gender: { name: string };
+        email: string;
+        subscribe: boolean;
+      }
+    ]
+  >({ url: "/api/v1/users" });
   return (
     <Main>
       <Head>
         <title>Helo, I'm Satchel.</title>
         <meta name="description" content="Satchel is a bla bla bla..." />
       </Head>
-      Index
+      <ul style={{ paddingTop: 100, margin: 0 }}>
+        {users?.map(
+          ({
+            _id,
+            name: { first_name, last_name },
+            email,
+            gender: { name },
+            subscribe
+          }) => (
+            <li key={_id} style={{ marginBottom: 20 }}>
+              <div style={{ textTransform: "capitalize" }}>
+                <span>{first_name}</span> <span>{last_name}</span>
+              </div>
+              <div>{email}</div>
+              <div>{name}</div>
+              <div>{subscribe ? "Subsciber" : "None subscriber"}</div>
+            </li>
+          )
+        )}
+      </ul>
     </Main>
   );
 };
