0f128b41c8e75cda457e0d69cfc6a9634946a4f6
Taufik Oktama <oktamataufik@gmail.com>
2020-02-25 09:46:32 +0700

    Add env (#2)

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ module.exports = {
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
     SESSION_SECRET: process.env.SESSION_SECRET,
-    SESSION_SECRET_REFRESH: process.env.SESSION_SECRET_REFRESH
+    SESSION_SECRET_REFRESH: process.env.SESSION_SECRET_REFRESH,
+    SHOPIFY_API: process.env.SHOPIFY_API,
+    SHOPIFY_API_SECRET: process.env.SHOPIFY_API_SECRET
   }
 };
